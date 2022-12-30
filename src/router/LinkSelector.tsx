8179e8c31af74c8b7ad53d5789ec776bebee2c6c
Jyonyve <keyneco@gmail.com>
2022-12-30 17:06:29 +0900

    로그인 기능 구현 중

--- src/router/LinkSelector.tsx
+++ src/router/LinkSelector.tsx
@@ -6,6 +6,7 @@ export const LinkSelector = (props:any) => {
         <nav className="navtop">
             <h2><NavLink to= '/'> Let's travel! club </NavLink></h2>
             <ul className="nav-links">
+                <li><NavLink to ='/login'>Login</NavLink></li>
                 <li><NavLink to = '/club'>Travel Clubs</NavLink></li>
                 <li><NavLink to ='/member'>For Members</NavLink></li>
             </ul>
